75e061232623de9574784ddbc13b31b3f48a6a1e
Ricky Tanudjaja CT <ricky.tanudjaja@cartrack.com>
2022-05-19 12:13:40 +0700

    start unicafe part 5

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,5 +1,5 @@
-const Header = ({ course }) => (
-  <h1>{course}</h1>
+const Header = ({ name }) => (
+  <h1>{name}</h1>
 )
 
 const Total = ({ parts }) => (
@@ -20,27 +20,30 @@ const Content = ({ parts }) => (
 )
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
