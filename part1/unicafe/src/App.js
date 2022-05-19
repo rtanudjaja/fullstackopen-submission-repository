a7a2f52cedd96168485f0adacebc73a7487d2fb4
Ricky Tanudjaja CT <ricky.tanudjaja@cartrack.com>
2022-05-19 12:08:08 +0700

    start unicafe part 3

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -3,7 +3,7 @@ const Header = ({ course }) => (
 )
 
 const Total = ({ content }) => (
-  <p>Number of exercises {content.exercises1 + content.exercises2 + content.exercises3}</p>
+  <p>Number of exercises {content.part1.exercises + content.part2.exercises + content.part3.exercises}</p>
 )
 
 const Part = ({ part, exercise }) => (
@@ -13,27 +13,30 @@ const Part = ({ part, exercise }) => (
 
 const Content = ({ content }) => (
   <div>
-    <Part part={content.part1} exercise={content.exercises1}/>
-    <Part part={content.part2} exercise={content.exercises2}/>
-    <Part part={content.part3} exercise={content.exercises3}/>
+    <Part part={content.part1.name} exercise={content.part1.exercises}/>
+    <Part part={content.part2.name} exercise={content.part2.exercises}/>
+    <Part part={content.part3.name} exercise={content.part3.exercises}/>
   </div>
 )
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
   const content = {
     part1,
     part2,
     part3,
-    exercises1,
-    exercises2,
-    exercises3
   }
 
   return (
