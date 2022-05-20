7494cc95a7e8897bbf933b5f98c61bd9b2ff737e
Ricky Tanudjaja CT <ricky.tanudjaja@cartrack.com>
2022-05-20 16:33:28 +0700

    finalise courseInfo

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,7 +1,51 @@
-const App = () => (
+const Header = ({ name }) => (
+  <h1>{name}</h1>
+)
+
+const Total = ({ parts }) => (
+  <p>Number of exercises {parts.reduce((acc, n) => acc + n.exercises, 0)}</p>
+)
+
+const Part = ({ name, exercise }) => (
+  <p>{name} {exercise}</p>
+)
+
+
+const Content = ({ parts }) => (
   <div>
-    <p>Hello world</p>
+    {parts.map(part => (
+      <Part key={part.name} name={part.name} exercise={part.exercises}/>
+    ))}
   </div>
 )
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
 
 export default App
